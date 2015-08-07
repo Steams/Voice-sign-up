100e3b1ab05dc87a544a73f335048d11cb8ec658
Steams <radclifferobs@gmail.com>
2015-08-07 15:24:37 -0500

    added typing animation

--- app/bundle.js
+++ app/bundle.js
@@ -156,7 +156,239 @@ angular.module("voice-signup").directive('formComp',['reactDirective',function(r
   angular.module("voice-signup").controller('sign-up_Ctrl', [
     '$scope', 'Fields_factory', function($scope, fields_fact) {
       var commands, x;
+      $scope.thing = "checking";
+      $scope.word = "";
+      $scope.keyboard = {
+        rows: [
+          {
+            rowNumber: 1,
+            rowKeys: [
+              {
+                keyValue: "q",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "w",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "e",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "r",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "t",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "y",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "u",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "i",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "o",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "p",
+                keyClass: "",
+                keyMod: ""
+              }
+            ]
+          }, {
+            rowNumber: 2,
+            rowKeys: [
+              {
+                keyValue: "a",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "s",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "d",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "f",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "g",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "h",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "j",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "k",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "l",
+                keyClass: "",
+                keyMod: ""
+              }
+            ]
+          }, {
+            rowNumber: 3,
+            rowKeys: [
+              {
+                keyValue: "^",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "z",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "x",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "c",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "v",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "b",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "n",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "m",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: ",",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: ".",
+                keyClass: "",
+                keyMod: ""
+              }, {
+                keyValue: "<--",
+                keyClass: "",
+                keyMod: ""
+              }
+            ]
+          }, {
+            rowNumber: 4,
+            rowKeys: [
+              {
+                keyValue: " ",
+                keyClass: "mod-space",
+                keyMod: ""
+              }
+            ]
+          }
+        ]
+      };
       $scope.label = "start";
+      $scope.unPress = function(key) {
+        var i, j, k, ref, results, row, x;
+        results = [];
+        for (x = j = 0, ref = $scope.keyboard.rows.length; j < ref; x = j += 1) {
+          row = $scope.keyboard.rows[x];
+          results.push((function() {
+            var m, ref1, results1;
+            results1 = [];
+            for (i = m = 0, ref1 = row.rowKeys.length; m < ref1; i = m += 1) {
+              k = row.rowKeys[i].keyValue;
+              if (k === key || k.toUpperCase() === key) {
+                console.log($scope.keyboard.rows[x].rowKeys[i].keyValue);
+                $scope.keyboard.rows[x].rowKeys[i].keyMod = "";
+                results1.push($scope.$apply());
+              } else {
+                results1.push(void 0);
+              }
+            }
+            return results1;
+          })());
+        }
+        return results;
+      };
+      $scope.space = function() {
+        $scope.$apply();
+        $scope.word += " ";
+        return setTimeout(function() {
+          return $scope.unPress(" ");
+        }, 200);
+      };
+      $scope.backspace = function() {
+        $scope.$apply();
+        $scope.word = $scope.word.slice(0, -1);
+        return setTimeout(function() {
+          return $scope.unPress("<--");
+        }, 200);
+      };
+      $scope.press = function(key) {
+        var i, j, k, m, ref, ref1, row, x;
+        for (x = j = 0, ref = $scope.keyboard.rows.length; j < ref; x = j += 1) {
+          row = $scope.keyboard.rows[x];
+          for (i = m = 0, ref1 = row.rowKeys.length; m < ref1; i = m += 1) {
+            k = row.rowKeys[i].keyValue;
+            if (k === key || k.toUpperCase() === key) {
+              console.log($scope.keyboard.rows[x].rowKeys[i].keyValue);
+              $scope.keyboard.rows[x].rowKeys[i].keyMod = "pressed";
+              if (key === "<--") {
+                console.log("backspace");
+                $scope.backspace();
+                return 0;
+              }
+              if (key === " ") {
+                $scope.space();
+                return 0;
+              }
+              $scope.word += key;
+              setTimeout(function() {
+                return $scope.unPress(key);
+              }, 200);
+              $scope.$apply();
+            }
+          }
+        }
+      };
+      $scope.type = function(word) {
+        var l, typeSpeed, x;
+        console.log("typing " + word);
+        x = 0;
+        l = word.length;
+        return typeSpeed = setInterval(function() {
+          var c;
+          if (x < l) {
+            c = word.charAt(x);
+            console.log("typing " + c);
+            $scope.press(c);
+            return x++;
+          } else {
+            return clearInterval(typeSpeed);
+          }
+        }, 100);
+      };
       $scope.fields = function() {
         return fields_fact.fields;
       };
@@ -175,7 +407,24 @@ angular.module("voice-signup").directive('formComp',['reactDirective',function(r
         fields_fact.getNext();
         return console.log(fields_fact.getIndex());
       };
-      return commands = {
+      $scope.undo = function() {
+        var l, typeSpeed;
+        console.log("ndoing");
+        x = 0;
+        l = $scope.word.length;
+        return typeSpeed = setInterval(function() {
+          console.log("interval");
+          if (x < l) {
+            $scope.press("<--");
+          } else {
+            clearInterval(typeSpeed);
+            $scope.unPress("<--");
+          }
+          x++;
+          return $scope.$apply();
+        }, 150);
+      };
+      commands = {
         'next': function() {
           console.log("called");
           $scope.start();
@@ -183,8 +432,38 @@ angular.module("voice-signup").directive('formComp',['reactDirective',function(r
         },
         'one': function() {
           return alert("works");
+        },
+        'type *name': function(name) {
+          console.log("typing");
+          return $scope.type(name);
+        },
+        'undo': function() {
+          $scope.undo();
+          return $scope.$apply();
         }
       };
+      document.getElementById("prompt-input").onkeydown = function(evt) {
+        var charCode;
+        evt = evt || window.event;
+        charCode = evt.keyCode || evt.which;
+        console.log(charCode);
+        switch (charCode) {
+          case 8:
+            evt.preventDefault();
+            return $scope.press("<--");
+        }
+      };
+      document.onkeypress = function(evt) {
+        var charCode, charStr;
+        evt.preventDefault();
+        evt = evt || window.event;
+        charCode = evt.keyCode || evt.which;
+        console.log(charCode);
+        charStr = String.fromCharCode(charCode);
+        return $scope.press(charStr);
+      };
+      annyang.addCommands(commands);
+      return annyang.start();
     }
   ]);
 
--- src/modules/sign-up/sign-up_Ctrl.coffee
+++ src/modules/sign-up/sign-up_Ctrl.coffee
@@ -3,7 +3,130 @@ require('../../lib/annyang.min.js')
 
 angular.module "voice-signup"
     .controller 'sign-up_Ctrl',['$scope','Fields_factory',($scope,fields_fact)->
+        $scope.thing ="checking"
+        $scope.word = ""
+        $scope.keyboard = {
+            rows : [
+                {
+                    rowNumber:1,
+                    rowKeys:[
+                        {keyValue:"q", keyClass:"", keyMod:""},
+                        {keyValue:"w", keyClass:"", keyMod:""},
+                        {keyValue:"e", keyClass:"", keyMod:""},
+                        {keyValue:"r", keyClass:"", keyMod:""},
+                        {keyValue:"t", keyClass:"", keyMod:""},
+                        {keyValue:"y", keyClass:"", keyMod:""},
+                        {keyValue:"u", keyClass:"", keyMod:""},
+                        {keyValue:"i", keyClass:"", keyMod:""},
+                        {keyValue:"o", keyClass:"", keyMod:""},
+                        {keyValue:"p", keyClass:"", keyMod:""},
+                    ]
+                },
+                {
+                    rowNumber:2,
+                    rowKeys:[
+                        {keyValue:"a", keyClass:"", keyMod:""},
+                        {keyValue:"s", keyClass:"", keyMod:""},
+                        {keyValue:"d", keyClass:"", keyMod:""},
+                        {keyValue:"f", keyClass:"", keyMod:""},
+                        {keyValue:"g", keyClass:"", keyMod:""},
+                        {keyValue:"h", keyClass:"", keyMod:""},
+                        {keyValue:"j", keyClass:"", keyMod:""},
+                        {keyValue:"k", keyClass:"", keyMod:""},
+                        {keyValue:"l", keyClass:"", keyMod:""},
+                    ]
+                },
+                {
+                    rowNumber:3,
+                    rowKeys:[
+                        {keyValue:"^", keyClass:"", keyMod:""},
+                        {keyValue:"z", keyClass:"", keyMod:""},
+                        {keyValue:"x", keyClass:"", keyMod:""},
+                        {keyValue:"c", keyClass:"", keyMod:""},
+                        {keyValue:"v", keyClass:"", keyMod:""},
+                        {keyValue:"b", keyClass:"", keyMod:""},
+                        {keyValue:"n", keyClass:"", keyMod:""},
+                        {keyValue:"m", keyClass:"", keyMod:""},
+                        {keyValue:",", keyClass:"", keyMod:""},
+                        {keyValue:".", keyClass:"", keyMod:""},
+                        {keyValue:"<--", keyClass:"", keyMod:""},
+                    ]
+                },
+                {
+                    rowNumber:4,
+                    rowKeys:[
+                        {keyValue:" ", keyClass:"mod-space", keyMod:""},
+                    ]
+                }
+            ]
+            
+        }
+
         $scope.label = "start"
+
+        $scope.unPress = (key)->
+            for x in [0...$scope.keyboard.rows.length] by 1
+                row = $scope.keyboard.rows[x]
+                for i in [0...row.rowKeys.length] by 1
+                    k = row.rowKeys[i].keyValue
+                    if(k == key || k.toUpperCase() == key)
+                        console.log $scope.keyboard.rows[x].rowKeys[i].keyValue
+                        $scope.keyboard.rows[x].rowKeys[i].keyMod = ""
+                        $scope.$apply()
+
+        $scope.space = ()->
+            $scope.$apply()
+            $scope.word += " "
+            setTimeout(()->
+                $scope.unPress(" ")
+            ,200
+            )
+
+        $scope.backspace = ()->
+                $scope.$apply()
+                $scope.word = $scope.word.slice(0,-1)
+                setTimeout(()->
+                    $scope.unPress("<--")
+                ,200
+                )
+
+        $scope.press = (key)->
+            for x in [0...$scope.keyboard.rows.length] by 1
+                row = $scope.keyboard.rows[x]
+                for i in [0...row.rowKeys.length] by 1
+                    k = row.rowKeys[i].keyValue
+                    if(k == key || k.toUpperCase() == key)
+                        console.log $scope.keyboard.rows[x].rowKeys[i].keyValue
+                        $scope.keyboard.rows[x].rowKeys[i].keyMod = "pressed"
+                        if(key == "<--")
+                            console.log "backspace"
+                            $scope.backspace()
+                            return 0
+                        if(key == " ")
+                            $scope.space()
+                            return 0
+                        $scope.word += key
+                        setTimeout(()->
+                            $scope.unPress(key)
+                        ,200
+                        )
+                        $scope.$apply()
+
+        $scope.type = (word)->
+            console.log "typing "+word
+            x =0
+            l = word.length
+            typeSpeed = setInterval(()->
+                if(x < l)
+                    c = word.charAt(x)
+                    console.log "typing "+c
+                    $scope.press(c)
+                    x++
+                else
+                    clearInterval(typeSpeed)
+            ,100
+            )
+
         $scope.fields = ()-> return fields_fact.fields
         $scope.init = ()->
             $scope.current_field = fields_fact.active()
@@ -22,6 +145,22 @@ angular.module "voice-signup"
             fields_fact.getNext()
             console.log fields_fact.getIndex()
 
+        $scope.undo = ()->
+            console.log "ndoing"
+            x = 0
+            l = $scope.word.length
+            typeSpeed = setInterval(()->
+                console.log "interval"
+                if(x < l)
+                    $scope.press("<--")
+                else
+                    clearInterval(typeSpeed)
+                    $scope.unPress("<--")
+                x++
+                $scope.$apply()
+            ,150
+            )
+
         commands = {
             'next' : ()->
                 console.log "called"
@@ -31,8 +170,40 @@ angular.module "voice-signup"
             'one' :()->
                 alert("works")
 
+            'type *name': (name)->
+                console.log "typing"
+                $scope.type(name)
+
+            'undo': ()->
+                $scope.undo()
+                $scope.$apply()
+
         }
-        #annyang.addCommands(commands)
-        #annyang.start()
+
+        #setTimeout(()->
+            #$scope.type("Radcliffe")
+        #,1000
+        #)
+
+        document.getElementById("prompt-input").onkeydown = (evt)->
+            evt = evt || window.event
+            charCode = evt.keyCode || evt.which
+            console.log charCode
+            switch charCode
+                when 8 then (evt.preventDefault()
+                $scope.press("<--")
+                )
+
+
+        document.onkeypress = (evt)->
+            evt.preventDefault()
+            evt = evt || window.event
+            charCode = evt.keyCode || evt.which
+            console.log charCode
+            charStr = String.fromCharCode(charCode)
+            $scope.press(charStr)
+        
+        annyang.addCommands(commands)
+        annyang.start()
 
 ]
